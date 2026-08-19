8ce81ec8975998183dc71910ffb8bef7bd9e47a4
Mellony JvRensburg <mellonymail@gmail.com>
2026-08-19 12:59:29 +0200

    Update LaptopCoverBot.java

--- src/main/java/com/bot/LaptopCoverBot.java
+++ src/main/java/com/bot/LaptopCoverBot.java
@@ -12,8 +12,11 @@ public class LaptopCoverBot {
         String MY_PHONE = "27783437797";
         
         try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-            Page page = browser.newPage();
+            Browser browser = playwright.chromium().launch(
+        new BrowserType.LaunchOptions()
+             .setHeadless(true)
+             .setArgs(Arrays.asList("--no-sandbox", "--disable-setuid-sandbox"))
+             );
             
             System.out.println("Hunting laptop covers...");
             page.navigate(searchUrl);
